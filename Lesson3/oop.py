fd16ae298d1d0c8ca543ee8af0a23e0e99cc52a2
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-10-26 10:54:51 +0700

    Lesson 3

--- Lesson3/oop.py
+++ Lesson3/oop.py
@@ -6,3 +6,26 @@ class User:
 
     def display_info(self):
         return f"{self.username} - {self.email} - {self.password}"
+    
+    # import file để đọc ghi dữ liệu
+import data_io
+
+class UserDatabase:
+    def __init__(self, file_path):
+        # file_path: đường dẫn đến file lưu trữ dữ liệu 
+        self.file_path = file_path
+        # Danh sách dạng object
+        self.users_list = list()
+        # Danh sách dạng dictionary
+        self.users_dict = data_io.load_json_data(self.file_path)
+
+
+    # Chuyển đổi dữ liệu từ json sang object
+    def load_data(self):
+        new_users = []
+        for user_data in self.users_dict:
+            user = User(username = user_data["username"],
+                        email = user_data["email"],
+                        password = user_data["password"])
+            new_users.append(user)
+        self.users_list = new_users
