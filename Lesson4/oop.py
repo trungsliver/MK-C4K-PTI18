8e7bc5d4390e349274e45012d5483a807666a5a9
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-02 10:59:22 +0700

    Lesson 4

--- Lesson4/oop.py
+++ Lesson4/oop.py
@@ -28,3 +28,33 @@ Worth:      {self.worth}
             if value:
                 setattr(self, key, value)
             
+class PlayerDatabase:
+    def __init__(self, file_path):
+        # filepath: đường dẫn đến file dữ liệu
+        self.file_path = file_path
+        # Danh sách dạng object Player
+        self.players_list = list()
+        # Danh sách dạng dictionary
+        self.players_dict = data_io.load_json_data(file_path)
+
+    # Chuyển đổi từ dict sang object Player
+    def convert_to_object(self):
+        new_players = []
+        for player_data in self.players_dict:
+            player = Player(id = player_data["id"],
+                            name = player_data['name'],
+                            dob = player_data['dob'],
+                            region = player_data['region'],
+                            club = player_data['club'],
+                            rating = player_data['rating'],
+                            worth = player_data['worth'])
+            new_players.append(player)
+        self.players_list = new_players
+
+    # Chuyển từ object => dictionary / json
+    def convert_to_dict(self):
+        json_data = list()
+        for player in self.players_list:
+            json_data.append(player.__dict__)
+        return json_data
+
